88ca86b083e90257170c14877a6400290ffd6247
7hanatos13@gmail.com <7hanatos13@gmail.com@9d6944dc-c2c8-9d0e-e0db-fd1f5c95fe5b>
2011-05-03 10:12:27 +0000

    Thêm các lớp kế thừa từ TinRaoVatDTO

--- trunk/Code/DTO/TinRaoVatDTO.cs
+++ trunk/Code/DTO/TinRaoVatDTO.cs
@@ -7,13 +7,55 @@ namespace DTO
 {
     public class TinRaoVatDTO
     {
-        private int _maTinRaoVat;
-        private DateTime _thoiGianDang;
-        private int ThoiHanLuuTin;
-        private DiaDiemDTO _diaDiem;
-        private int _soLanXem;
-        private NguoiDungDTO nguoiDung;
-        private DanhMucConDTO _danhMucCon;
-        private bool _deleted;
+        protected int _maTinRaoVat;
+        protected DateTime _thoiGianDang;
+        protected int _thoiHanLuuTin;
+        protected DiaDiemDTO _diaDiem;
+        protected int _soLanXem;
+        protected NguoiDungDTO _nguoiDung;
+        protected DanhMucConDTO _danhMucCon;
+        protected bool _deleted;
+
+        public int MaTinRaoVat
+        {
+            get { return _maTinRaoVat; }
+            set { _maTinRaoVat = value; }
+        }
+        public DateTime ThoiGianDang
+        {
+            get { return _thoiGianDang; }
+            set { _thoiGianDang = value; }
+        }
+        public int ThoiHanLuuTin
+        {
+            get { return _thoiHanLuuTin; }
+            set { _thoiHanLuuTin = value; }
+        }
+        public DiaDiemDTO DiaDiem
+        {
+            get { return _diaDiem; }
+            set { _diaDiem = value; }
+        }        
+        public int SoLanXem
+        {
+            get { return _soLanXem; }
+            set { _soLanXem = value; }
+        }
+        public NguoiDungDTO NguoiDung
+        {
+            get { return _nguoiDung; }
+            set { _nguoiDung = value; }
+        }
+        public DanhMucConDTO DanhMucCon
+        {
+            get { return _danhMucCon; }
+            set { _danhMucCon = value; }
+        }
+        public bool Deleted
+        {
+            get { return _deleted; }
+            set { _deleted = value; }
+        }
     }
+
 }
