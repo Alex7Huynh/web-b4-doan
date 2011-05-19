03388e6e5ca4d0b1b19f1731bedaedda93683f8a
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@9d6944dc-c2c8-9d0e-e0db-fd1f5c95fe5b>
2011-05-19 10:18:09 +0000

    sửa lỗi thiếu file. author: 0812517.

---
+++ Code/B4-RaoVat/DanhMuc/XemThongTinLienhe.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BUS;
+using DAO;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            prevPage = Request.UrlReferrer.ToString();
+        }
+
+        int maNguoiDung = 1;
+        string id = Request.QueryString["id"];
+        Int32.TryParse(id, out maNguoiDung);
+
+        NGUOIDUNG nguoiDung = NguoiDungBUS.LayNguoiDungTheoMa(maNguoiDung);
+
+        lblTên.Text = nguoiDung.TenNguoiDung;
+        lblEmail.Text = nguoiDung.Email;
+        lblDiaChi.Text = nguoiDung.DiaChi;
+        lblDienThoai.Text = nguoiDung.DienThoai;
+    }
+
+    static string prevPage = String.Empty;
+
+    protected void btnQuayLai_Click(object sender, EventArgs e)
+    {
+        Response.Redirect(prevPage);
+    }
+}
