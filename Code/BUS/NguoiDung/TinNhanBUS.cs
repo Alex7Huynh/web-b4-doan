bca87721e4aa5c9d6d4902d2e0fbfeee86f8ff73
thuan178@gmail.com <thuan178@gmail.com@9d6944dc-c2c8-9d0e-e0db-fd1f5c95fe5b>
2011-05-25 08:56:30 +0000

    chuc nang tin nhan

---
+++ Code/BUS/NguoiDung/TinNhanBUS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAO;
+
+namespace BUS
+{
+    public class TinNhanBUS
+    {
+        public static List<TINNHAN> LayDanhSachTinNhan()
+        {
+            return TinNhanDAO.LayDanhSachTinNhan();
+        }
+    }
+}
