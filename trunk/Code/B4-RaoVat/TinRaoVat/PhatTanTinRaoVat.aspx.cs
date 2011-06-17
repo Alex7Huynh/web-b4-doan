8add850fef4fbd4669ab5e4b37918ea71584e8d4
thuan178@gmail.com <thuan178@gmail.com@9d6944dc-c2c8-9d0e-e0db-fd1f5c95fe5b>
2011-06-17 08:12:05 +0000

    xong phát tán tin rao vặt,commit wai k đc,chán

---
+++ trunk/Code/B4-RaoVat/TinRaoVat/PhatTanTinRaoVat.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using BUS;
+using DAO;
+
+public partial class XemDanhSachTinRaoVat : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        RSS rss = new RSS();
+        rss.NewRSS();
+        RSS.RssChannel channel = new RSS.RssChannel();
+
+        channel.Title = "Website Rao Vặt";
+        channel.Link = "http://localhost:3866/B4-RaoVat/Default.aspx";
+        channel.Description = "Website Đăng tin rao vặt.";
+        rss.AddRssChannel(channel);
+
+        RSS.RssItem item = new RSS.RssItem();
+        item.Title = "Home";
+        item.Link = "http://localhost:3866/B4-RaoVat/Default.aspx";
+        item.Description = "Trang chủ tin rao vặt.";
+        rss.AddRssItem(item);
+
+        RSS.RssItem item1 = new RSS.RssItem();
+        item1.Title = "Xem nội dung tin rao vặt.";
+        item1.Link = "http://localhost:3866/B4-RaoVat/DanhMuc/XemNoiDungTin.aspx";
+        item1.Description = "Các tin rao vặt được đăng mới nhất";
+        rss.AddRssItem(item1);
+        
+        Response.Clear();
+        Response.ContentType = "text/xml";
+        Response.Write(rss.RssDocument);
+        Response.End();
+    }   
+}
