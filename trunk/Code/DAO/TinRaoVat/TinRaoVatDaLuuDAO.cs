9409ea54e36ebc9d6db207e5c02fe2aaba8379df
thuan178@gmail.com <thuan178@gmail.com@9d6944dc-c2c8-9d0e-e0db-fd1f5c95fe5b>
2011-06-18 07:04:33 +0000

    commit thiếu,sory

---
+++ trunk/Code/DAO/TinRaoVat/TinRaoVatDaLuuDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace DAO
+{
+    public class TinRaoVatDaLuuDAO
+    {
+        /// <summary>
+        /// Insert new TINRAOVAT
+        /// </summary>
+        /// <param name="tinRaoVat"></param>
+        /// <returns></returns>
+        public static bool ThemTinRaoVatDaLuu(TINRAOVATDALUU tinRaoVatDaLuu)
+        {
+            try
+            {
+                RaoVatDataClassesDataContext db = new RaoVatDataClassesDataContext();
+                db.TINRAOVATDALUUs.InsertOnSubmit(tinRaoVatDaLuu);
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            { return false; }
+
+            return true;
+
+        }
+
+        /// <summary>
+        /// Delete old TINRAOVAT
+        /// </summary>
+        /// <param name="maTinRaoVat"></param>
+        /// <returns></returns>
+        public static bool XoaTinRaoVatViPham(int maTinRaoVatViPham)
+        {
+            try
+            {
+                RaoVatDataClassesDataContext db = new RaoVatDataClassesDataContext();
+                LICHSUTINRAOVATVIPHAM tinRaoVatViPham = db.LICHSUTINRAOVATVIPHAMs.Single(t => t.MaLichSuTinRaoVatViPham == maTinRaoVatViPham);
+                tinRaoVatViPham.deleted = true;
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            { return false; }
+
+            return true;
+        }
+
+    }
+}
